e0dda7db41a568d2e3cebfe4bf2771c3f0fa8317
mohammadmahdi <hamdimohammadmahdi@gmail.com>
2024-06-03 14:47:30 +0330

    Created a directory and create a txt file

---
+++ app/src/main/java/com/example/file/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.file
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.file.databinding.ActivityMainBinding
+import java.io.File
+import java.io.FileWriter
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.createFolder.setOnClickListener {
+            val fileDir = File(getExternalFilesDir(null), "testDirectory")
+            if (!fileDir.exists()) {
+                fileDir.mkdirs()
+                Toast.makeText(this, "پوشه ساخته شد", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "پوشه قبلا وجود داشته است", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        val fileDir = File(getExternalFilesDir(null), "testDirectory")
+        val textFile = File(fileDir, "file.txt")
+        binding.createTextFile.setOnClickListener {
+            val fileWriter = FileWriter(textFile , true)
+            fileWriter.append("سلام")
+            fileWriter.flush()
+            fileWriter.close()
+            Toast.makeText(this, "فایل متنی ساخته شد", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
